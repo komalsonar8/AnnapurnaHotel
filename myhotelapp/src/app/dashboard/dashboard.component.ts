42b55c0b2921d85ea2558f8da8b9e1ee8cf13b46
komalsonar8 <komal.sonar8@gmail.com>
2022-03-18 15:19:16 +0000

    add Details

--- myhotelapp/src/app/dashboard/dashboard.component.ts
+++ myhotelapp/src/app/dashboard/dashboard.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder } from '@angular/forms';
+import { subscribeOn } from 'rxjs';
+import { ApiService } from '../shared/api.service';
+import { RestaurantData } from '../shared/restaurant.model';
 
 @Component({
   selector: 'app-dashboard',
@@ -9,7 +12,8 @@ import { FormGroup, FormBuilder } from '@angular/forms';
 export class DashboardComponent implements OnInit {
 
   formValue!: FormGroup
-  constructor(private formBuilder: FormBuilder) { }
+  restaurantmodelobject:RestaurantData=new RestaurantData;
+  constructor(private formBuilder: FormBuilder , private api:ApiService) { }
 
   ngOnInit(): void {
     this.formValue = this.formBuilder.group({
@@ -19,7 +23,29 @@ export class DashboardComponent implements OnInit {
       address: [''],
       services: ['']
     })
-
+    
   }
+
+
+    //subscribing data
+    addResto(){
+      this.restaurantmodelobject.name=this.formValue.value.name;
+      this.restaurantmodelobject.email=this.formValue.value.email;
+      this.restaurantmodelobject.mobile=this.formValue.value.mobile;
+      this.restaurantmodelobject.address=this.formValue.value.address;
+      this.restaurantmodelobject.services=this.formValue.value.services;
+
+      this.api.postRestaurant(this.restaurantmodelobject).subscribe(res=>{
+        console.log(res);
+        alert("Restaurant Record Added Successfully");
+      },
+      err=>{
+        alert("Something Went Wrong!!!!");
+
+      }
+      )
+      
+    }
+  
 
 }
